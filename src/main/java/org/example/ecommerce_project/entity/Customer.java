082d29c71429dc764bec0725e11c6394c88b5424
David Refai <Davidm.refai@hotmail.com>
2025-12-23 14:19:17 +0100

    feat: create customer entity implementation with validation for email (closes #12)

--- src/main/java/org/example/ecommerce_project/entity/Customer.java
+++ src/main/java/org/example/ecommerce_project/entity/Customer.java
@@ -1,4 +1,60 @@
 package org.example.ecommerce_project.entity;
 
-public class Customer {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "customer")
+public class Customer{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Email must not be blank")
+    @Email(message = "Invalid email address format")
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public void setOrders(List<Order> orders) {
+        this.orders = orders;
+    }
 }
