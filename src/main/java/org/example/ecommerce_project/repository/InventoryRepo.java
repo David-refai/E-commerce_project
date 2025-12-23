b9dbbdc0a72e5036fc02c4c96f136e67ec8e6fee
adnansattar80@gmail.com <adnansattar80@gmail.com>
2025-12-23 15:47:24 +0100

    Refactored all Repositories

--- src/main/java/org/example/ecommerce_project/repository/InventoryRepo.java
+++ src/main/java/org/example/ecommerce_project/repository/InventoryRepo.java
@@ -1,5 +1,7 @@
 package org.example.ecommerce_project.repository;
 
+import org.example.ecommerce_project.entity.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
--- src/main/java/org/example/ecommerce_project/repository/OrderItemRepo.java
+++ src/main/java/org/example/ecommerce_project/repository/OrderItemRepo.java
@@ -1,6 +1,10 @@
 package org.example.ecommerce_project.repository;
 
+import org.example.ecommerce_project.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderItemRepo extends JpaRepository<OrderItem, Long> {
