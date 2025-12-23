799273400b3f01d0874b9712bb879323422c6573
adnansattar80@gmail.com <adnansattar80@gmail.com>
2025-12-23 14:20:06 +0100

    Refactored all Repositories

--- src/main/java/org/example/ecommerce_project/repository/InventoryRepo.java
+++ src/main/java/org/example/ecommerce_project/repository/InventoryRepo.java
@@ -1,4 +1,7 @@
 package org.example.ecommerce_project.repository;
 
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface InventoryRepo extends JpaRepository<Inventory, Long> {
 }
--- src/main/java/org/example/ecommerce_project/repository/ProductRepo.java
+++ src/main/java/org/example/ecommerce_project/repository/ProductRepo.java
@@ -1,11 +1,13 @@
 package org.example.ecommerce_project.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ProductRepo extends JpaRepository<Product, Long> {
     Optional<Product> findBySku(String sku);
 
