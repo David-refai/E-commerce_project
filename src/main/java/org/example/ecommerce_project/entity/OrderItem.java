1704bdcddd38b75af4ebdd77b47fa8b5174d3606
David Refai <Davidm.refai@hotmail.com>
2025-12-23 15:12:38 +0100

    feat: create OrderItem entity implementation with validation (closes #9)

---
+++ src/main/java/org/example/ecommerce_project/entity/OrderItem.java
@@ -0,0 +1,86 @@
+package org.example.ecommerce_project.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Min(value = 1)
+    @Column(nullable = false)
+    private int qty;
+
+    @NotNull
+    @Column(name = "unit_price", nullable = false, precision = 12, scale = 2)
+    private BigDecimal unitPrice;
+
+    @Column(name="line_total", nullable=false, precision=12, scale=2)
+    private BigDecimal lineTotal;
+
+    @PrePersist @PreUpdate
+    void calcTotals() {
+        this.lineTotal = unitPrice
+                .multiply(BigDecimal.valueOf(qty))
+                .setScale(2, RoundingMode.HALF_UP);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public BigDecimal getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(BigDecimal unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public BigDecimal getLineTotal() {
+        return lineTotal;
+    }
+}
--- src/main/java/org/example/ecommerce_project/repository/PaymentRepo.java
+++ src/main/java/org/example/ecommerce_project/repository/PaymentRepo.java
@@ -1,7 +1,11 @@
 package org.example.ecommerce_project.repository;
 
 
+import org.example.ecommerce_project.entity.Payment;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface PaymentRepo extends JpaRepository<Payment, Long> {
